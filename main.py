96ad3efda351a142015a5054c29444ecff1ccce5
darthvega7 <50564378+darthvega7@users.noreply.github.com>
2024-03-20 20:10:51 -0500

    v0.0.1: 03/20/2024
    Bare-bones. GUI can interact with the back-end.

---
+++ main.py
@@ -0,0 +1,59 @@
+import tkinter as tk
+import trainer
+
+def nextSplit():
+    dealer_card, player_card, answer = trainer.playSplits(False)
+    dealerCard.config(text="Dealer Card: " + str(dealer_card))
+    playerCard.config(text="Player Card: " + str(player_card))
+    correctAnswer.config(text="Correct Answer: " + str(answer))
+
+def nextSoft():
+    dealer_card, player_card, answer = trainer.playSofts(False)
+    dealerCard.config(text="Dealer Card: " + str(dealer_card))
+    playerCard.config(text="Player Card: " + str(player_card))
+    correctAnswer.config(text="Correct Answer: " + str(answer))
+
+def nextHard():
+    dealer_card, player_card, answer = trainer.playHards(False)
+    dealerCard.config(text="Dealer Card: " + str(dealer_card))
+    playerCard.config(text="Player Card: " + str(player_card))
+    correctAnswer.config(text="Correct Answer: " + str(answer))
+
+def changeToSplits():
+    nextBtn.config(command=nextSplit)
+def changeToSofts():
+    nextBtn.config(command=nextSoft)
+
+def changeToHards():
+    nextBtn.config(command=nextHard)
+
+window = tk.Tk()
+window.title("Joshy's BlackJack Basic Strategy Trainer")
+window.geometry("1920x1080")
+window.geometry("+0+0")
+icon = tk.PhotoImage(file="bitcoin.png")
+window.iconphoto(True, icon)
+
+splitsBtn = tk.Button(window, text="Play Splits", command=changeToSplits)
+splitsBtn.place(x=650, y=25)
+
+softsBtn = tk.Button(window, text="Play Soft Totals", command=changeToSofts)
+softsBtn.place(x=800, y=25)
+
+hardsBtn = tk.Button(window, text="Play Hard Totals", command=changeToHards)
+hardsBtn.place(x=1000, y=25)
+
+
+dealerCard = tk.Label(window, text="Dealer Card: ", font=("Arial", 20))
+dealerCard.place(x=800, y=100)
+
+playerCard = tk.Label(window, text="Player Card: ", font=("Arial", 20))
+playerCard.place(x=800, y=150)
+
+correctAnswer = tk.Label(window, text="Correct Answer: ", font=("Arial", 20))
+correctAnswer.place(x=800, y=200)
+
+nextBtn = tk.Button(window, text="Next Card", command=nextSplit)
+nextBtn.place(x=800, y=300)
+
+window.mainloop()
