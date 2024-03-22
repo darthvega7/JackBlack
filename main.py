672c850adc666730591df51c5d91770eac63bbb1
darthvega7 <50564378+darthvega7@users.noreply.github.com>
2024-03-22 16:24:42 -0500

    v0.0.3: 03/22/2024
    Added all the action buttons (hit, stand, Ds, Dh) and mode buttons (Play splits, softs, hards). Current mode disables mode button. Split mode only shows split options. Still need to figure out in the All mode if on splits or not.

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import tkinter as tk
 import trainer
 
+checkAnswer = ""
+
 def nextSplit():
+    global checkAnswer
     dealer_card, player_card, answer = trainer.playSplits(False)
     dealerCard.config(text="Dealer Card: " + str(dealer_card))
     playerCard.config(text="Player Card: " + str(player_card))
     correctAnswer.config(text="Correct Answer: " + str(answer))
+    checkAnswer = answer
 
 def nextSoft():
     dealer_card, player_card, answer = trainer.playSofts(False)
@@ -26,15 +30,91 @@ def nextAll():
     correctAnswer.config(text="Correct Answer: " + str(answer))
 
 def changeToSplits():
-    nextBtn.config(command=nextSplit)
+    splitsBtn.config(relief=tk.SUNKEN, state=tk.DISABLED)
+    softsBtn.config(relief=tk.RAISED, state=tk.NORMAL)
+    hardsBtn.config(relief=tk.RAISED, state=tk.NORMAL)
+    allBtn.config(relief=tk.RAISED, state=tk.NORMAL)
+    hitBtn.place_forget()
+    standBtn.place_forget()
+    doubleHitBtn.place_forget()
+    doubleStandBtn.place_forget()
+    splitYesBtn.place(x=800, y=300)
+    splitNoBtn.place(x=900, y=300)
+    nextSplit()
 def changeToSofts():
-    nextBtn.config(command=nextSoft)
+    splitsBtn.config(relief=tk.RAISED, state=tk.NORMAL)
+    softsBtn.config(relief=tk.SUNKEN, state=tk.DISABLED)
+    hardsBtn.config(relief=tk.RAISED, state=tk.NORMAL)
+    allBtn.config(relief=tk.RAISED, state=tk.NORMAL)
+    splitYesBtn.place_forget()
+    splitNoBtn.place_forget()
+    hitBtn.place(x=720, y=300)
+    standBtn.place(x=775, y=300)
+    doubleHitBtn.place(x=850, y=300)
+    doubleStandBtn.place(x=975, y=300)
+    nextSoft()
 
 def changeToHards():
-    nextBtn.config(command=nextHard)
-
+    splitsBtn.config(relief=tk.RAISED, state=tk.NORMAL)
+    softsBtn.config(relief=tk.RAISED, state=tk.NORMAL)
+    hardsBtn.config(relief=tk.SUNKEN, state=tk.DISABLED)
+    allBtn.config(relief=tk.RAISED, state=tk.NORMAL)
+    splitYesBtn.place_forget()
+    splitNoBtn.place_forget()
+    hitBtn.place(x=720, y=300)
+    standBtn.place(x=775, y=300)
+    doubleHitBtn.place(x=850, y=300)
+    doubleStandBtn.place(x=975, y=300)
+    nextHard()
 def changeToAll():
-    nextBtn.config(command=nextAll)
+    splitsBtn.config(relief=tk.RAISED, state=tk.NORMAL)
+    softsBtn.config(relief=tk.RAISED, state=tk.NORMAL)
+    hardsBtn.config(relief=tk.RAISED, state=tk.NORMAL)
+    allBtn.config(relief=tk.SUNKEN, state=tk.DISABLED)
+    nextAll()
+def checkSplitYes():
+    print("Split Yes")
+    if(checkAnswer == "Y"):
+        print("Correct")
+    else:
+        print("Incorrect. Correct answer is Do Not Split")
+    nextSplit()
+
+def checkSplitNo():
+    print("Split No")
+    if(checkAnswer == "N"):
+        print("Correct")
+    else:
+        print("Incorrect. Correct answer is Split")
+    nextSplit()
+
+def checkHit():
+    print("Hit")
+    if(checkAnswer == "H"):
+        print("Correct")
+    else:
+        print("Incorrect. Correct answer is: " + checkAnswer)
+
+def checkStand():
+    print("Stand")
+    if(checkAnswer == "S"):
+        print("Correct")
+    else:
+        print("Incorrect. Correct answer is: " + checkAnswer)
+
+def checkDh():
+    print("Double or Hit")
+    if(checkAnswer == "Dh"):
+        print("Correct")
+    else:
+        print("Incorrect. Correct answer is: " + checkAnswer)
+
+def checkDs():
+    print("Double or Stand")
+    if(checkAnswer == "Ds"):
+        print("Correct")
+    else:
+        print("Incorrect. Correct answer is: " + checkAnswer)
 
 window = tk.Tk()
 window.title("Joshy's BlackJack Basic Strategy Trainer")
@@ -64,7 +144,15 @@ playerCard.place(x=800, y=150)
 correctAnswer = tk.Label(window, text="Correct Answer: ", font=("Arial", 20))
 correctAnswer.place(x=800, y=200)
 
-nextBtn = tk.Button(window, text="Next Card", command=nextSplit)
-nextBtn.place(x=800, y=300)
+# nextBtn = tk.Button(window, text="Next Card", command=nextSplit)
+# nextBtn.place(x=800, y=300)
+
+splitYesBtn = tk.Button(window, text="Split", command=checkSplitYes)
+splitNoBtn = tk.Button(window, text="Do Not Split", command=checkSplitNo)
+
+hitBtn = tk.Button(window, text="Hit", command=checkHit)
+standBtn = tk.Button(window, text="Stand", command=checkStand)
+doubleHitBtn = tk.Button(window, text="Double or Hit", command=checkDh)
+doubleStandBtn = tk.Button(window, text="Double or Stand", command=checkDs)
 
 window.mainloop()
